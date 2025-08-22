6689f12887786f08f44546632fc8f1c6ab91b607
TheCrazy8 <bennettbashford@gmail.com>
2025-08-22 10:41:53 -0400

    s

--- import PyInstaller.py
+++ import PyInstaller.py
@@ -2,14 +2,8 @@ import PyInstaller.__main__
 import os
 from pathlib import Path
 
-# Get the path to the user's home directory
-home_directory = Path.home()
-
-# Construct the path to the Downloads directory
-downloads_directory = home_directory / "Downloads"
-
 # Change the current working directory to the Downloads directory
-os.chdir(downloads_directory)
+os.chdir(r"C:\Users\s977168\Downloads")
 
 print(f"Successfully navigated to: {os.getcwd()}")
 
