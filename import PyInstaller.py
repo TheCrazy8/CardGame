b9559b4890f7789358ca4e25d997d9131353a2b7
TheCrazy8 <bennettbashford@gmail.com>
2025-08-22 10:42:38 -0400

    s

--- import PyInstaller.py
+++ import PyInstaller.py
@@ -2,14 +2,20 @@ import PyInstaller.__main__
 import os
 from pathlib import Path
 
+# Get the path to the user's home directory
+home_directory = Path.home()
+
+# Construct the path to the Downloads directory
+downloads_directory = home_directory / "Downloads"
+
 # Change the current working directory to the Downloads directory
-os.chdir(r"C:\Users\s977168\Downloads")
+os.chdir(downloads_directory)
 
 print(f"Successfully navigated to: {os.getcwd()}")
 
 # Run the script to make Python file into an EXE file
 PyInstaller.__main__.run([
-'"Card Game.py"',
+'Card Game.py',
 '--add-data "images:images"',
 '--add-data "card_game_saves.json:."',
 '--add-data "card_game_leaderboard.json:."',
